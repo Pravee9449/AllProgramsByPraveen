bc42962f43f13a1db9db44a385e60242e3ae0a35
Pravee <Pravee@Praveen>
2022-11-21 10:17:16 +0530

    all programs added

---
+++ src/test/java/allprograms/MixedPrograms.java
@@ -0,0 +1,154 @@
+package allprograms;
+
+
+
+
+public class MixedPrograms {
+
+	public static void main(String[] args) {
+		
+//		package basic;
+//
+//		public class AllCodesKalpana {
+//
+//			package practice;
+//
+//			import org.openqa.selenium.By;
+//			import org.openqa.selenium.WebDriver;
+//			import org.openqa.selenium.chrome.ChromeDriver;
+//			import org.openqa.selenium.firefox.FirefoxDriver;
+//
+//			public class AllCodesHere {
+//
+//				public static void main(String[] args) {
+//					String key="webdriver.chrome.driver";
+//				    String value= "./drivers/chromedriver.exe";
+//				    System.setProperty(key, value);
+//				    ChromeDriver driver= new ChromeDriver();
+//				    Thread.sleep(5000);
+//				    driver.close();
+//				   // driver.quit();
+//				    
+//				public static void main(String[] args) {
+//				    String key1 = "webdriver.chrome.driver";
+//				   	String value1 = "./drivers/chromedriver.exe";
+//				   	System.setProperty(key1, value1);
+//				   	ChromeDriver driver= new ChromeDriver();
+//
+//				   	String key2 = "webdriver.gecko.driver";
+//			    	String value2 = "./drivers/geckodriver.exe";
+//			    	System.setProperty(key2, value2);
+//				    FirefoxDriver driver2= new FirefoxDriver();
+//				    	
+//				    String key="webdriver.chrome.driver";
+//					String value="./drivers/chromedriver.exe";
+//					System.setProperty(key, value);
+//					WebDriver driver= new ChromeDriver();
+//					driver.get("https://www.facebook.com/");
+//					Thread.sleep(4000);
+//					String title=driver.getTitle();
+//					System.out.println(title);
+//					String url=driver.getCurrentUrl();
+//					System.out.println(url);
+//					String source=driver.getPageSource();
+//					System.out.println(source);
+//					driver.close();	
+//						
+//					String key="webdriver.chrome.driver";
+//					String value="./drivers/chromedriver.exe";
+//					System.setProperty(key, value);
+//					WebDriver driver= new ChromeDriver();
+//					driver.get("https://www.flipkart.com/?ef_id=244f0739d6521b6e836e65da9e233f20:G:s&s_kwcid=AL!739!10!76484920232329!76485137405439&semcmpid=sem_F1167BY7_Brand_adcenter");
+//					driver.close();	
+//
+//					//String key= "webdriver.chrome.driver";
+//				    //String value="./drivers/chromedriver.exe";
+//					//System.setProperty(key, value);
+//					System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//				    WebDriver driver= new ChromeDriver();
+//				    driver.get("https://www.facebook.com/");
+//				    driver.findElement(By.id("email")).click();
+//				    Thread.sleep(5000);
+//				    driver.findElement(By.name("pass")).click();
+//				    Thread.sleep(5000);
+//				    // driver.findElement(By.name("login")).click();	
+//					    
+//				    //String key= "webdriver.chrome.driver";
+//				    //String value="./drivers/chromedriver.exe";
+//					//System.setProperty(key, value);
+//					System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//				    WebDriver driver= new ChromeDriver();
+//				    driver.get("https://www.facebook.com/");
+//				    driver.findElement(By.id("email")).sendKeys("9449708286");
+//				    Thread.sleep(5000);
+//				    driver.findElement(By.name("pass")).sendKeys("8884209957@Pr");
+//				    Thread.sleep(5000);
+//				    driver.findElement(By.name("login")).click();  
+//					    
+//					    
+//					System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//				    WebDriver driver= new ChromeDriver();
+//				    driver.get("file:///C:/Users/Pravee/OneDrive/Desktop/Selenium%20html/classNameLocator.html");
+//				    Thread.sleep(3000);
+//				    //driver.findElement(By.tagName("input")).sendKeys("Ravi");
+//				    // driver.findElement(By.name("n1")).sendKeys("Praveen");
+//				    // driver.findElement(By.id("a1")).sendKeys("Hii guys");
+//				    //driver.findElement(By.className("c1")).click();
+//				    driver.findElement(By.cssSelector("input[id='a1']")).click();
+//				    Thread.sleep(2000);
+//				    driver.findElement(By.cssSelector("[id='a1']")).sendKeys("i am Praveen");
+//				    System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//				    WebDriver driver= new ChromeDriver();
+//				    driver.get("file:///C:/Users/Pravee/OneDrive/Desktop/Selenium%20html/cssexpr.html");
+//				    Thread.sleep(3000);
+//				    driver.findElement(By.tagName("input")).sendKeys("Ravi");
+//				    // driver.findElement(By.name("n1")).sendKeys("Praveen");
+//				    // driver.findElement(By.id("a1")).sendKeys("Hii guys");
+//				    Thread.sleep(5000);
+//				    driver.close();
+//				    
+//				    
+//				    System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//				    WebDriver driver= new ChromeDriver();
+//				    driver.get("file:///C:/Users/Pravee/OneDrive/Desktop/Selenium%20html/cssexp1.html");
+//				    Thread.sleep(3000);
+//				    //driver.findElement(By.tagName("input")).sendKeys("Ravi");
+//				    // driver.findElement(By.name("n1")).sendKeys("Praveen");
+//				    // driver.findElement(By.id("a1")).sendKeys("Hii guys");
+//				    //WebElement ele=driver.findElement(By.tagName("input"));
+//				    //ele.clear();
+//				    // driver.findElement(By.tagName("input")).clear();
+//				    driver.findElement(By.cssSelector("input[type='text']")).clear();
+//				    Thread.sleep(5000);
+//				    driver.close();
+		
+//		//public class CustomWaitPractice
+//		public static void main(String[] args) {
+//			WebDriverManager.chromedriver().setup();
+//			WebDriver driver = new ChromeDriver();
+//			driver.manage().window().maximize();
+//			driver.get("https://www.amazon.com/");
+//
+//			System.out.println(driver.getTitle());
+//			driver.findElement(By.id("twotabsearchtextbox")).sendKeys("phone");
+//			int count = 0;
+//			// Custom Wait
+//			while (count < 20) {
+//				try {
+//					driver.findElement(By.id("nav-search-submit-button")).click();
+//					break;
+//				} catch (Exception e) {
+//					System.out.println("Element not found");
+//				}
+//			}
+
+				}
+
+			
+
+			
+
+		
+	}
+
+
